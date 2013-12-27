2a48a120d540b401639a9d6acc0f5dc18a278b03
Agustin Ramos <agustin.ramos@knowtion.biz>
2013-12-27 16:51:33 -0600

    Added bouncing

--- Site.elm
+++ Site.elm
@@ -24,10 +24,10 @@ La agenda del evento es la siguiente:
 |:------:|-|-|:-----:|-|-|:----------
 |10:00   | | |10:30  | | | Apertura
 |10:30   | | |11:30  | | | "Introducción al cálculo lambda" por @sergiodnila
-|11:30   | | |13:00  | | | Presentaciones de soluciones a los problemas
+|11:30   | | |13:00  | | | Presentaciones de soluciones al problema 1
 |13:00   | | |14:00  | | | "Introducción a matemática" por @lux_spes
 |14:00   | | |15:00  | | | Comida
-|15:00   | | |16:30  | | | Presentaciones de soluciones a los problemas
+|15:00   | | |16:30  | | | Presentaciones de soluciones al problema 2
 |16:30   | | |17:30  | | | "Aplicaciones masivamente concurrentes" por @hiphoox
 |17:30   | | |18:00  | | | Cierre
 
@@ -61,6 +61,7 @@ recibirán un premio sorpresa el día del evento ;-)
 defaultRadius    = 350
 defaultTxtWidth  = 500
 defaultTxtHeight = 1200
+defaultLogoSize  = 200
 
 -- Aux functions
 
@@ -79,8 +80,8 @@ indexedSin i time = sin <| (time * pi / 4000) + toFloat(i) * 3  * pi / 5
 
 -- Images
 
-lambdaImg = image 200 200 "img/LambdaMx.png"     |> toForm
-alonzoImg = image 200 200 "img/AlonzoChurch.png" |> toForm
+lambdaImg = image defaultLogoSize defaultLogoSize "img/LambdaMx.png"     |> toForm
+alonzoImg = image defaultLogoSize defaultLogoSize "img/AlonzoChurch.png" |> toForm
 
 logos = map (image 120 120) [
   "img/clojure.png",
@@ -96,18 +97,23 @@ logos = map (image 120 120) [
 
 -- Page Model
 
-data State = Approaching | Spinning | Inviting
+data State = Approaching | Spinning | Redirecting | Inviting
 
 type Point = (Float, Float)
-type Logo  = { img: Form, pos: Point, scale: Float }
+type Logo  = { img: Form, pos: Point, scale: Float, vx: Float, vy: Float }
 type Page  = { center: Logo, logos: [Logo], content: Form, state:State }
-type Input = { click: Bool, delta: Time, pos: Point, dim: (Int,Int) }
+type Input = { click: Bool, ttime: Time, delta: Time, pos: Point, dim: (Int,Int) }
 
 initialPage : Page
 initialPage = 
   let idxLogos       = zip [0..(length logos)-1] logos
-      moveLogo (i,l) = { img = l, pos = positionFor i (length logos) 0, scale = 1 }
-  in { center  = { img = alonzoImg, pos = (0,0), scale = 1 }
+      moveLogo (i,l) = { img = l
+                       , pos = positionFor i (length logos) 0
+                       , scale = 1
+                       , vx = 0
+                       , vy = 0 
+                       }
+  in { center  = { img = alonzoImg, pos = (0,0), scale = 1, vx = 0, vy = 0 }
      , logos   = idxLogos |> map moveLogo
      , content = invitation |> width defaultTxtWidth 
                             |> container defaultTxtWidth defaultTxtHeight midTop 
@@ -117,13 +123,14 @@ initialPage =
      }
 
 stepPage : Input -> Page -> Page
-stepPage {click, delta, pos, dim} ({center, logos, content, state} as page) = 
+stepPage {click, ttime, delta, pos, dim} ({center, logos, content, state} as page) = 
   let newState = if | state == Approaching && click -> Spinning
-                    | state == Spinning && click    -> Inviting
+                    | state == Spinning && click    -> Redirecting
+                    | state == Redirecting          -> Inviting
                     | otherwise                     -> state
   in { page | content <- stepContent newState content
             , center  <- stepCenter  newState center  pos dim
-            , logos   <- stepLogos   newState delta   pos dim logos
+            , logos   <- stepLogos   newState logos   pos dim ttime delta
             , state   <- newState
      }
 
@@ -140,20 +147,42 @@ stepCenter state logo pos dim =
     Approaching -> { logo | scale <- newScale }
     Spinning    -> { logo | scale <- newScale, img <- lambdaImg }
     Inviting    -> { logo | img <- scale 0 logo.img }
+    Redirecting -> { logo | img <- scale 0 logo.img }
 
-stepLogos : State -> Time -> Point -> (Int, Int) -> [Logo] -> [Logo]
-stepLogos state time pos dim logos = 
+stepLogos : State -> [Logo] -> Point -> (Int, Int) -> Time -> Time -> [Logo]
+stepLogos state logos pos dim time delta  = 
   let newAlpha            = 1 - distance2Center pos (asPoint dim)
       toAlpha logo        = { logo | img <- logo.img |> alpha newAlpha }
       toAlpha25 logo      = { logo | img <- logo.img |> alpha 0.25 }
       idxLogos            = zip [0..(length logos)-1] logos
-      moveLogo (i,logo)   = (i, { logo | pos <-  positionFor i (length logos) time })
+      spin (i,logo)       = (i, { logo | pos <-  positionFor i (length logos) time })
       resizeLogo (i,logo) = (i, { logo | scale <- 1 + 0.2 * (indexedSin i time) })
       unindex (i,logo)    = logo
+      angle (r,theta)     = theta
+      toLinearMov logo    = { logo | vx <- 150 * cos ((angle . toPolar) logo.pos) 
+                                   , vy <- 150 * sin ((angle . toPolar) logo.pos) }
+      xpos logo           = fst logo.pos
+      ypos logo           = snd logo.pos
+      halfWindowWidth     = toFloat (fst dim) / 2
+      halfWindowHeight    = toFloat (snd dim) / 2
+      bounceMargin logo   = defaultLogoSize * logo.scale / 2
+      moveLinear logo     = { logo | pos <- ( xpos logo + logo.vx * delta , 
+                                              ypos logo + logo.vy * delta )
+                                   , vx <- stepV logo.vx (xpos logo > (halfWindowWidth-(bounceMargin logo))) 
+                                                         (xpos logo < -(halfWindowWidth-(bounceMargin logo)))
+                                   , vy <- stepV logo.vy (ypos logo > (halfWindowHeight-(bounceMargin logo))) 
+                                                         (ypos logo < -(halfWindowHeight-(bounceMargin logo)))
+                            }
   in case state of
-    Approaching -> logos    |> map toAlpha
-    Spinning    -> idxLogos |> map (unindex . moveLogo . resizeLogo) 
-    Inviting    -> idxLogos |> map (unindex . moveLogo . resizeLogo) |> map toAlpha25
+    Approaching  -> logos    |> map toAlpha
+    Spinning     -> idxLogos |> map (unindex . spin . resizeLogo) 
+    Inviting     -> idxLogos |> map (unindex . resizeLogo) |> map (moveLinear . toAlpha25)
+    Redirecting  -> logos    |> map toLinearMov
+
+stepV v upperCollision lowerCollision =
+  if | lowerCollision -> abs v
+     | upperCollision -> 0 - abs v
+     | otherwise      -> v
 
 positionFor : Int -> Int -> Time -> Point
 positionFor i n time = fromPolar (defaultRadius, indexedRotationAngle i n time)
@@ -166,11 +195,14 @@ display (w,h) {center, logos, content, state} =
   in collage w (max h defaultTxtHeight) <| 
      draw center :: (map draw logos) ++ [content]
 
-delta = foldp (+) 0 (fps 30)
-input = sampleOn delta <| Input <~ Mouse.isDown
-                                 ~ delta
-                                 ~ (asPoint <~ Mouse.position)
-                                 ~ Window.dimensions
+input = 
+  let source = fps 30
+      ttime  = foldp (+) 0 source
+  in sampleOn ttime <| Input <~ Mouse.isDown
+                              ~ ttime
+                              ~ (inSeconds <~ source)
+                              ~ (asPoint <~ Mouse.position)
+                              ~ Window.dimensions
 main = 
   let pageState = foldp stepPage initialPage input
   in display <~ Window.dimensions ~ pageState
