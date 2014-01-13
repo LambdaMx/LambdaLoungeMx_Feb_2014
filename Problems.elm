7e7d4a08b892dcb20c939e726d2dd7158181c597
Agustin Ramos <agustin.ramos@knowtion.biz>
2014-01-13 15:48:56 -0600

    Added problem descriptions. Fixed markdown

---
+++ Problems.elm
@@ -0,0 +1,75 @@
+
+content = [markdown|
+# Problemas del Lambda Lounge Mx 2014
+
+## Problema 1: Peg Game
+
+Existe un viejo juego en el que una pelota (usualmente de metal) se deja caer 
+desde la parte superior de un tablero vertical, escogiendo una de varias 
+posiciones desde donde soltarla. En el tablero hay pijas (obstáculos) contra 
+los que la pelota rebotará conforme cae por efecto de la gravedad. Siempre que 
+la pelota choca contra una pija, rebotará hacia la izquierda o la derecha con 
+igual probabilidad (0.5). La única excepción es cuando choca contra una pija 
+que se encuentra en el extremo derecho o izquierdo del tablero, en cuyo caso 
+siempre rebota en dirección hacia el centro del tablero. Cuando nuestro tablero 
+fue creado, tenía todas las pijas en su lugar. Sin embargo, es un tablero viejo 
+y ahora le faltan algunas pijas. El objetivo del juego, es lograr que la pelota 
+caiga hasta el fondo del tablero en un lugar específico.
+
+Tu tarea es, dado un tablero de juego, determinar el lugar óptimo para soltar 
+la pelota, tal que maximice la probabilidad de que caiga en un lugar específico.
+
+La siguiente figura muestra un ejemplo de un tablero con 5 filas y 5 columnas. 
+Nota que la fila superior tiene 5 pijas, la siguiente tiene 4, la siguiente 5, 
+etc. Con 5 columnas, existen 4 opciones dónde dejar caer la pelota (indexando 
+desde 0). Nota que en este ejemplo, faltan 3 pijas. Por convención, diremos que 
+la fila de la parte superior es la fila 0, y la pija que se encuentra más a la 
+izquierda se encuentra en la columna 0, de tal manera que las pijas faltantes 
+se encuentran en (1,1), (2,1) y (3,2). En este ejemplo, el mejor lugar para 
+soltar la pelota es en la columna 1 (la que está más a la izquierda), la cual 
+te da un 50% de probabilidad de que caiga en el objetivo.
+
+>  x.x.x.x.x 
+>
+>   x...x.x 
+>
+>  x...x.x.x 
+>
+>   x.x...x 
+>
+>  x.x.x.x.x
+>
+>   G   
+
+'x' indica una pija, '.' indica espacio vacío.
+
+**Entradas:**
+
+1. Número de filas y columnas, (filas impares)
+2. Posicion de pijas faltantes (no hay faltantes en 1a fila)
+3. Columna de salida    
+
+**Salida:**
+
+Las columna con más probabilidad de ganar.
+
+
+## Problema 2: Twitter network
+
+Utilizando el API de twitter, crear un grafo donde los nodos representen a las
+personas que sigues o te siguen, y donde el peso de las aristas representen la 
+fuerza del vínculo, representada por menciones, retuits, y favoritos.
+
+**Entradas:**
+
+1. Un nombre de usuario de twitter
+2. Un token de autentificación.
+
+**Salida:**
+
+Un grafo (en la notación que gustes) representando tu red social en 
+twitter y la fuerza de cada una de estas relaciones.
+
+|]
+
+main = content |> width 600
---
+++ Spinning.elm
@@ -0,0 +1,31 @@
+import Mouse
+import Keyboard
+import Window
+import Graphics.Element
+
+alonzoImg = image 200 200 "img/AlonzoChurch.png" |> toForm
+
+logos = map (image 120 120) [
+  "img/clojure.png", 
+  "img/elixir.png", 
+  "img/elm.png", 
+  "img/erlang.png", 
+  "img/fsharp.png", 
+  "img/haskell.png", 
+  "img/mathematica.png",
+  "img/scheme.png",
+  "img/scala.png"
+  ] |> map toForm
+
+logoWheel time =
+  let alonzo = alonzoImg |> move (0,0) 
+      toRotatingLogo n angle = 
+        drop n logos
+        |> head
+        |> scale (1 + 0.2 * (sin <| (time * pi / 4000) + toFloat(n) * pi / 10 ))
+        |> move (fromPolar (350, angle + pi - (time * pi / 6000)))    
+  in collage 900 900 <| alonzo :: map (\n -> toRotatingLogo n (turns (toFloat(n)/9))) [0..8] 
+
+animation = lift logoWheel (foldp (+) 0 (fps 30))
+
+main = animation 
