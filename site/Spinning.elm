943abb67f878f8ed7678af889410b77a8e4d2c58
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-07-27 12:36:22 -0500

    Updated Spinning.elm to elm-platform 0.15.1

--- site/Spinning.elm
+++ site/Spinning.elm
@@ -1,7 +1,12 @@
-import Mouse
-import Keyboard
-import Window
-import Graphics.Element
+import Mouse exposing(..)
+import Keyboard exposing(..)
+import Window exposing(..)
+import List exposing(..)
+import Signal exposing((<~), foldp)
+import Time exposing(fps)
+import Color exposing(..)
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
 
 alonzoImg = image 200 200 "img/AlonzoChurch.png" |> toForm
 
@@ -17,15 +22,20 @@ logos = map (image 120 120) [
   "img/scala.png"
   ] |> map toForm
 
+headForm xs =
+  case (head xs) of
+    Just x -> x
+    _      -> circle 0.1 |> filled white
+
 logoWheel time =
   let alonzo = alonzoImg |> move (0,0) 
       toRotatingLogo n angle = 
         drop n logos
-        |> head
+        |> headForm
         |> scale (1 + 0.2 * (sin <| (time * pi / 4000) + toFloat(n) * pi / 10 ))
         |> move (fromPolar (350, angle + pi - (time * pi / 6000)))    
   in collage 900 900 <| alonzo :: map (\n -> toRotatingLogo n (turns (toFloat(n)/9))) [0..8] 
 
-animation = lift logoWheel (foldp (+) 0 (fps 30))
+animation = logoWheel <~ (foldp (+) 0 (fps 30))
 
 main = animation 
